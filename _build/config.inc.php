04efe647780808b492564a27c104c249fc99769c
Alexey <info@createit.ru>
2023-11-28 12:54:22 +0300

    Updated to 1.0.1-pl

--- _build/config.inc.php
+++ _build/config.inc.php
@@ -11,7 +11,7 @@ if (!defined('MODX_CORE_PATH')) {
 return [
     'name' => 'easyRedirects',
     'name_lower' => 'easyredirects',
-    'version' => '1.0.0',
+    'version' => '1.0.1',
     'release' => 'pl',
     // Install package to site right after build
     'install' => true,
